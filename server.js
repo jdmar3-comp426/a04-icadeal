e2091dc5e2d06e7c390fe998f96d3126857365df
icadeal <calebrd@live.unc.edu>
2021-11-29 10:59:39 -0500

    attempted again to implement delete

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.patch("/app/update/user:id", (req, res) => {
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
-	const stmt = db.prepare("DELETE FROM userinfo WHERE id=?");
-	const info = stmt.run(req.params.id);
+	const stmt = db.prepare("DELETE FROM userinfo", (req))
+	const info = stmt.run();
 	console.log(info.changes)
 })
 // Default response for any other request
