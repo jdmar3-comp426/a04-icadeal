1f530f882abfbfb607d096adf376678378f27558
icadeal <calebrd@live.unc.edu>
2021-11-29 10:51:29 -0500

    attempted to implement CREATE new user

--- server.js
+++ server.js
@@ -24,8 +24,9 @@ app.get("/app/", (req, res, next) => {
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new", (req, res) => {
-	const stmt = db.prepare("INSERT INTO userinfo", (req)).all()
-	res.status(200).json(stmt);
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)")
+	const info = stmt.run('test', 'pass');
+	console.log(info.changes)
 })
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
