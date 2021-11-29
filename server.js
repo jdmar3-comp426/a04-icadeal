f6e4cacbd43c9d2f0a717f3a6b85884fdb82330b
icadeal <calebrd@live.unc.edu>
2021-11-29 16:46:51 -0500

    formatted new user message

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post("/app/new", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)")
 	const info = stmt.run(req.body.user, md5(req.body.pass));
 	res.json({"message":`1 record created: ID 3 (201)`})
-	console.log(info.changes)
+	res.json({"id": `${req.body.id}`, "user": `${req.body.user}`, 'pass': `${req.body.pass}`})
 })
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
