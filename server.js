7294e5741e94dfc0f898ee619c3cab9da537c2d9
icadeal <calebrd@live.unc.edu>
2021-11-29 10:10:08 -0500

    attempted to implement READ a single user

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/users", (req, res) => {	
-	const stmt = db.prepare("", (req)).all();
+app.get("/app/users/:id", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo", (req)).all();
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
